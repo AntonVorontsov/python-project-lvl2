140a9e2d4d5739fedbc11eda29ce27a4e596f410
AntonVorontsov <vorontsovanton@gmail.com>
2022-09-06 17:00:30 +0300

    configiring GitHub actions

--- tests/test_create_diff.py
+++ tests/test_create_diff.py
@@ -3,10 +3,10 @@ from gendiff.gendiff import create_diff
 
 
 def test_create_diff():
-    with open('/Users/antonvorontsov/PycharmProjects/python-project-lvl2/tests/fixtures/create_diff_result.txt', 'r') as file:
+    with open('./tests/fixtures/create_diff_result.txt', 'r') as file:
         result = file.read()
 
-    assert str(create_diff('/Users/antonvorontsov/PycharmProjects/python-project-lvl2/gendiff/parsers/file1.json', '/Users/antonvorontsov/PycharmProjects/python-project-lvl2/gendiff/parsers/file2.json')) == result
+    assert str(create_diff('./gendiff/parsers/file1.json', './gendiff/parsers/file2.json')) == result
     # assert str(create_diff('gendiff/parsers/file1.yml', 'gendiff/parsers/file2.yml')) == result
     # assert str(create_diff('gendiff/parsers/file1.json', 'gendiff/parsers/file2.yml')) == result
 
