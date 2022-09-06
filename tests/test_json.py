e3f67e3be8c6eadb7e2f94d4adb3c1765582d479
AntonVorontsov <vorontsovanton@gmail.com>
2022-09-06 17:03:56 +0300

    configiring GitHub actions

--- tests/test_json.py
+++ tests/test_json.py
@@ -3,6 +3,6 @@ from gendiff.gendiff import generate_diff
 
 
 def test_json():
-    with open('/Users/antonvorontsov/PycharmProjects/python-project-lvl2/tests/fixtures/json_result.txt', 'r') as file:
+    with open('./tests/fixtures/json_result.txt', 'r') as file:
         result = file.read()
-    assert _json(generate_diff('/Users/antonvorontsov/PycharmProjects/python-project-lvl2/gendiff/parsers/file1.json', '/Users/antonvorontsov/PycharmProjects/python-project-lvl2/gendiff/parsers/file2.json', 'json')) == result
+    assert _json(generate_diff('.gendiff/parsers/file1.json', './gendiff/parsers/file2.json', 'json')) == result
