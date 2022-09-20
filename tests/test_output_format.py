00baa9c95f090d2d3295b84192bfc1219d273921
AntonVorontsov <vorontsovanton@gmail.com>
2022-09-20 10:07:50 +0300

    Fixing errors

--- tests/test_output_format.py
+++ tests/test_output_format.py
@@ -2,13 +2,11 @@ import pytest
 from gendiff.gendiff import generate_diff
 
 
-@pytest.mark.parametrize('test_input,expected',[
+@pytest.mark.parametrize('test_input,expected', [
     (('gendiff/parsers/file1.yaml', 'gendiff/parsers/file2.yaml', 'stylish'), 'tests/fixtures/result_stylish'),
     (('gendiff/parsers/file1.json', 'gendiff/parsers/file2.json', 'json'), 'tests/fixtures/result_json'),
     (('gendiff/parsers/file1.json', 'gendiff/parsers/file2.yaml', 'plain'), 'tests/fixtures/result_plain')
 ])
-
-
 def test_output(test_input, expected):
     with open(expected, 'r') as file:
         result = file.read()
